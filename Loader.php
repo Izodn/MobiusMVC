6fee05f5699034f5ebc3621bb9536288edd73f90
Brandon Burton <izodn0@gmail.com>
2015-06-03 09:20:04 -0700

    Creates a loader file to handle class includes

---
+++ Loader.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/Interfaces/Http/Request.php';
+require_once __DIR__ . '/Interfaces/Http/Response.php';
+require_once __DIR__ . '/Interfaces/Controller.php';
+
+require_once __DIR__ . '/Components/Http/Requests/BaseRequest.php';
+require_once __DIR__ . '/Components/Http/Requests/BasicRequest.php';
+require_once __DIR__ . '/Components/Http/Responses/BaseResponse.php';
+require_once __DIR__ . '/Components/Http/Responses/BasicResponse.php';
+require_once __DIR__ . '/Components/Http/Responses/JsonResponse.php';
+require_once __DIR__ . '/Components/Router/Route.php';
+require_once __DIR__ . '/Components/Router/RouteCollection.php';
+
+require_once __DIR__ . '/Application.php';
