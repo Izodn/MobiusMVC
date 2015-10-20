c6e14d3730df553951f7c824db3a35c0e256439a
Brandon Burton <izodn0@gmail.com>
2015-10-20 16:30:14 -0700

    Now really uses explode instead of split

--- Loader.php
+++ Loader.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function ($class) {
-	$pathArr = split('\\', $class);
+	$pathArr = explode('\\', $class);
 	$pathArrLen = count($pathArr);
 	$path = __DIR__;
 	for ($i = ($pathArrLen === 1 ? 0 : 1); $i < $pathArrLen; $i++) {
